c456c2867b044a94e028295a88ed1e486111613a
Ronaldo Magalhaes <ronaldo.magalhaes1@gmail.com>
2021-01-25 16:13:35 -0300

    Projeto iniciado mostrando a mensagem Ola Mundo

---
+++ OlaMundoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
