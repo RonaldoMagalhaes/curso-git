a5db9a99f6978c0b018bc3243e2eefd41dd29418
Ronaldo Magalhaes <ronaldo.magalhaes1@gmail.com>
2021-01-25 16:29:42 -0300

    aumentado mais uma linha no loop

--- OlaMundoGit/Program.cs
+++ OlaMundoGit/Program.cs
@@ -10,6 +10,12 @@ namespace OlaMundoGit
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está?");
 
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("oi");
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
